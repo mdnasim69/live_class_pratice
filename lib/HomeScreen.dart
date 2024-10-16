8dcc14766e169ea19d8afe85b29111a042c21a2e
mdnasim69 <mdnasimnasim2007@gmail.com>
2024-10-16 22:12:38 +0600

    changed

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,102 +7,47 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
       appBar: AppBar(
-        toolbarHeight: 50,
-        backgroundColor: Colors.greenAccent,
-        title: const Text(
-          'Nasim',
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
       ),
       body: Center(
         child: Column(
-          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            const Text(
-              '',
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blueAccent,
-                foregroundColor: Colors.black,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                shadowColor: Colors.green,
-                elevation: 10,
-                side: const BorderSide(color: Colors.yellow, width: 3.5),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(35)),
-                textStyle: const TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 30,
-                ),
-              ),
-              onPressed: () {},
-              child: const Text(
-                'nasim',
-              ),
-            ),
-            const Text(
-              '',
-            ),
-            GestureDetector(
-              onTap: () {
-                print('this is an image');
-              },
-              child: Image.asset(
-                'asset/images/download.jpeg',
-                fit: BoxFit.cover,
-                width: double.infinity,
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                print('i am fine');
-              },
-              child: const Text(
-                "hello all ,how are you all ? "
-                "i hope you are well",
-                style: TextStyle(fontSize: 26, color: Colors.cyanAccent),
-              ),
+            const Text('Hello all how are you?'),
+            const SizedBox(
+              height: 10,
             ),
             IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.code_sharp),
-              style: IconButton.styleFrom(
-                foregroundColor: Colors.yellow,
-                backgroundColor: Colors.green,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 35, vertical: 18),
-                iconSize: 50,
-                side: const BorderSide(color: Colors.black, width: 2),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-              ),
-            ),
+                style: IconButton.styleFrom(
+                    backgroundColor: Colors.white38,
+                    foregroundColor: Colors.black,
+                    side:
+                        const BorderSide(color: Colors.indigoAccent, width: 4),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 30),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    iconSize: 40),
+                onPressed: () {},
+                icon: const Icon(Icons.cached)),
             const SizedBox(
               height: 15,
             ),
             InkWell(
-              splashColor: Colors.cyanAccent,
+              splashColor: Colors.indigoAccent,
               onTap: () {
                 showDialog(
+                    barrierColor: Colors.yellow,
                     barrierDismissible: false,
-                    barrierColor: Colors.lightGreenAccent,
                     context: context,
                     builder: (context) {
                       return AlertDialog(
-                        title: const Text('Delete'),
-                        content: const Text(
-                          'Do you want to delet',
-                          style: TextStyle(fontSize: 20),
-                        ),
+                        title:
+                            const Text("nasim", style: TextStyle(fontSize: 20)),
+                        content: const Text('are you nasim',
+                            style: TextStyle(fontSize: 20)),
                         actions: [
-                          TextButton(
+                          ElevatedButton(
                               onPressed: () {
                                 Navigator.of(context).pop();
                               },
@@ -109,34 +55,127 @@ class HomeScreen extends StatelessWidget {
                                 'NO',
                                 style: TextStyle(fontSize: 20),
                               )),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text(
-                              'Yes',
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text(
+                                'YES',
+                                style: TextStyle(fontSize: 20),
+                              )),
                         ],
                       );
                     });
               },
               child: const Text(
-                'delete',
+                'nasim',
                 style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.brown,
-                ),
+                    fontSize: 25,
+                    color: Colors.green,
+                    fontWeight: FontWeight.w600),
               ),
-            )
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  side: const BorderSide(color: Colors.black, width: 2),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  backgroundColor: Colors.yellow,
+                  textStyle: const TextStyle(
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                onPressed: () {
+                  showModalBottomSheet(
+                      isScrollControlled: true,
+                      useSafeArea: true,
+                      enableDrag: true,
+                      elevation: 20,
+                      context: context,
+                      backgroundColor: Colors.yellow,
+                      //barrierColor: Colors.green.shade100,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7)),
+                      builder: (context) {
+                        return const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('nasim'),
+                            Text('nasim'),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text('nasim'),
+                                SizedBox(
+                                  width: 30,
+                                  height: 30,
+                                ),
+                                Text('nasim')
+                              ],
+                            ),
+                            Divider(
+                              height: 100,
+                            ),
+                          ],
+                        );
+                      });
+                },
+                child: const Text('ModalBottom')),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+              child: TextField(
+               maxLength:11,
+                obscureText: false,
+                maxLines: 1,
+                keyboardType: TextInputType.datetime,
+                decoration: InputDecoration(
+                    hintText: 'phone',
+                    labelText: 'phone number',
+                    labelStyle: const TextStyle(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    helperStyle: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey.shade100,
+                        fontSize: 25)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+              child: TextField(
+                maxLength: 50,
+                minLines: 1,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText:'email',
+                    labelText: 'Email address',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+              child: TextField(
+                maxLength: 16,
+                minLines: 1,
+                keyboardType: TextInputType.emailAddress,
+                obscureText:true,
+                decoration: InputDecoration(
+                    hintText:'password',
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+              ),
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.deepOrange,
-        child: const Icon(Icons.add),
       ),
     );
   }
