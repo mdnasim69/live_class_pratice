0c465e557dcba4dee0e805005d4ed7950a169d38
mdnasim69 <mdnasimnasim2007@gmail.com>
2024-10-17 19:47:35 +0600

    changed

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -12,7 +12,7 @@ class HomeActivity extends StatelessWidget {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Text('NASiM'),
+            Text('NASIM'),
           ],
         ),
         toolbarHeight: 60,
@@ -36,17 +36,21 @@ class HomeActivity extends StatelessWidget {
                       actions: [
                         OutlinedButton(
                             style: OutlinedButton.styleFrom(
-                                side: BorderSide(
-                                  color: Colors.black,
-                                  width: 2,
-                                ),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(5)),
-                                backgroundColor: Colors.blue,),
+                              side: BorderSide(
+                                color: Colors.black,
+                                width: 2,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)),
+                              backgroundColor: Colors.blue,
+                            ),
                             onPressed: () {
                               Navigator.of(context).pop();
                             },
-                            child: Text('No',style: TextStyle(color: Colors.black),)),
+                            child: Text(
+                              'No',
+                              style: TextStyle(color: Colors.black),
+                            )),
                         OutlinedButton(
                             style: OutlinedButton.styleFrom(
                                 side: BorderSide(width: 2, color: Colors.black),
@@ -56,7 +60,10 @@ class HomeActivity extends StatelessWidget {
                             onPressed: () {
                               Navigator.of(context).pop();
                             },
-                            child: Text('Yes',style: TextStyle(color: Colors.black),)),
+                            child: Text(
+                              'Yes',
+                              style: TextStyle(color: Colors.black),
+                            )),
                       ],
                     );
                   });
@@ -67,13 +74,16 @@ class HomeActivity extends StatelessWidget {
           IconButton(
             onPressed: () {
               showModalBottomSheet(
-                isScrollControlled:true,shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                  useSafeArea:true,
-                  enableDrag:true,
-                  backgroundColor:Colors.green,
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  useSafeArea: true,
+                  enableDrag: true,
+                  backgroundColor: Colors.green,
                   context: context,
                   builder: (context) {
-                    return const Column(children: [Row()],
+                    return const Column(
+                      children: [Row()],
                     );
                   });
             },
@@ -87,6 +97,102 @@ class HomeActivity extends StatelessWidget {
         ],
       ),
       drawer: Drawer(),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 25,
+            ),
+            Text(
+              'Loagin Information',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: false,
+                maxLength: 11,
+                maxLines: 1,
+                decoration: InputDecoration(
+                    hintText: 'Phone',
+                    labelText: 'Phone Number',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    prefixIcon: Icon(Icons.phone)),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: false,
+                maxLines: 1,
+                decoration: InputDecoration(
+                    hintText: 'Email',
+                    labelText: 'Email Address',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    prefixIcon: Icon(Icons.email)),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                maxLength: 16,
+                maxLines: 1,
+                decoration: InputDecoration(
+                    hintText: 'Password',
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    prefixIcon: Icon(Icons.password_outlined)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(11.5, 0, 11.5, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Sing up',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.redAccent,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+                        side: BorderSide(color: Colors.black, width: 2),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5))),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.greenAccent,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+                        side: BorderSide(color: Colors.black, width: 2),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5))),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
